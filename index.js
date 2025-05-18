14cba693b37a04ab31eb01d7337cca9db811f37a
hungngo <qhung.bm0409@gmail.com>
2025-05-18 19:24:18 +0700

    feat: arbitrum sepolia with order hash log and test

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const { ethers } = require("ethers");
 const domain = {
   name: "TdsContract",
   version: "1.0.0",
-  chainId: 1, // Matching the test environment chain ID
-  verifyingContract: "0x1Bcd6c83f3b2D2FC242a9223FFA0cbe43b272E35", // Contract address from test
+  chainId: 421614, // Matching the test environment chain ID
+  verifyingContract: "0xea510eEd83C4F08a69Bb425728B3523783693e7F", // Contract address from test
 };
 
 const userOrderTypes = {
@@ -51,6 +51,58 @@ async function signUserOrder() {
     console.log("Types:", userOrderTypes);
     console.log("UserOrder:", userOrder);
 
+    // Calculate EIP-712 domain separator
+    const domainSeparatorJS = ethers.utils._TypedDataEncoder.hashDomain(domain);
+    console.log("\nJS Domain Separator:", domainSeparatorJS);
+    const internalDomainHash =
+      ethers.utils._TypedDataEncoder.hashDomain(domain);
+    console.log("Internal Domain Hash:", internalDomainHash);
+    if (internalDomainHash !== domainSeparatorJS) {
+      throw new Error("InternalDomain hash mismatch");
+    }
+    const expectedDomainHash =
+      "0xe09f678c0990499685aa1fd3071947345fbe5f526e3f1e404415568ec4c0fef4";
+    console.log(
+      "Domain Hash Matches:",
+      domainSeparatorJS === expectedDomainHash
+    );
+
+    // Calculate EIP-712 struct hash for UserOrder
+    const structHashJS = ethers.utils._TypedDataEncoder.hashStruct(
+      "UserOrder",
+      userOrderTypes,
+      userOrder
+    );
+    const internalStructHash = ethers.utils._TypedDataEncoder
+      .from(userOrderTypes)
+      .hash(userOrder);
+    console.log("Internal Struct Hash:", internalStructHash);
+    if (internalStructHash !== structHashJS) {
+      throw new Error("InternalStruct hash mismatch");
+    }
+
+    const expectedStructHashJS =
+      "0x82e6f72dc84952903e1cadf5c7075cb11bacd4a6b9d6a39a2b5561195f989887";
+    console.log("JS Struct Hash:", structHashJS);
+    console.log(
+      "JS Struct Hash Matches:",
+      structHashJS === expectedStructHashJS
+    );
+
+    const expectedUserOrderHash =
+      "0xe8914527c3253c4446999aad0dfd2bee90f94da648bbaad27e8ee5f7945353be";
+    const userOrderHash = ethers.utils._TypedDataEncoder.hash(
+      domain,
+      userOrderTypes,
+      userOrder
+    );
+    console.log("UserOrder Hash:", userOrderHash);
+    console.log("Expected UserOrder Hash:", expectedUserOrderHash);
+    console.log(
+      "UserOrder Hash Matches:",
+      userOrderHash === expectedUserOrderHash
+    );
+
     const signature = await wallet._signTypedData(
       domain,
       userOrderTypes,
@@ -59,7 +111,7 @@ async function signUserOrder() {
     console.log("\nSignature:", signature);
 
     const expectedSignature =
-      "0xd9c723195d1ca33c804ecc340ec1825b8d8e70414635b241bde94490408b2cf00ce07b7f2a1976161384882e0eb7e7a55b670c603eec9c001a2b8687ed9355971c";
+      "0x11a7a0cd689334a0d6ef9039579a78ae57e71e32f90c28e1e8fbfc6d6f49c46963017ab1f06ac8f087a615e03df161683768d5da286fd3a836e51ec00fbee2a41b";
     console.log("Expected Signature:", expectedSignature);
     console.log("Signatures match:", signature === expectedSignature);
 
@@ -102,11 +154,61 @@ async function signOrder() {
     console.log("Types:", orderTypes);
     console.log("Order:", order);
 
+    const domainSeparatorJS = ethers.utils._TypedDataEncoder.hashDomain(domain);
+    console.log("\nJS Domain Separator:", domainSeparatorJS);
+    const internalDomainHash =
+      ethers.utils._TypedDataEncoder.hashDomain(domain);
+    console.log("Internal Domain Hash:", internalDomainHash);
+    if (internalDomainHash !== domainSeparatorJS) {
+      throw new Error("InternalDomain hash mismatch");
+    }
+    const expectedDomainHash =
+      "0xe09f678c0990499685aa1fd3071947345fbe5f526e3f1e404415568ec4c0fef4";
+    console.log(
+      "Domain Hash Matches:",
+      domainSeparatorJS === expectedDomainHash
+    );
+
+    const structHashJS = ethers.utils._TypedDataEncoder.hashStruct(
+      "Order",
+      orderTypes,
+      order
+    );
+    console.log("JS Struct Hash:", structHashJS);
+    const internalStructHash = ethers.utils._TypedDataEncoder
+      .from(orderTypes)
+      .hash(order);
+    console.log("Internal Struct Hash:", internalStructHash);
+    if (internalStructHash !== structHashJS) {
+      throw new Error("InternalStruct hash mismatch");
+    }
+    if (internalStructHash !== structHashJS) {
+      throw new Error("InternalStruct hash mismatch");
+    }
+    const expectedStructHashJS =
+      "0x86b265fe4e42471ce4d5a2611b6878626ea4236491a498b79ea8761f5efddfe2";
+    console.log("JS Struct Hash:", structHashJS);
+    console.log(
+      "JS Struct Hash Matches:",
+      structHashJS === expectedStructHashJS
+    );
+
+    const expectedOrderHash =
+      "0x7fb7f11b742eea78541fa27a03cdf6137403e7ed79c4174e5a1008949801ea39";
+    const orderHash = ethers.utils._TypedDataEncoder.hash(
+      domain,
+      orderTypes,
+      order
+    );
+    console.log("Order Hash:", orderHash);
+    console.log("Expected Order Hash:", expectedOrderHash);
+    console.log("Order Hash Matches:", orderHash === expectedOrderHash);
+
     const signature = await wallet._signTypedData(domain, orderTypes, order);
     console.log("\nSignature:", signature);
 
     const expectedSignature =
-      "0xcc9a7273f33fee7df107eefcd1ee02cc602f3ab5cd94865a18fed6ad01a9af9125620f9cc95aab96b247cb4d07c22167767f7dd1498619c18efbdb50a7c16e1b1b";
+      "0x10e509ab476004ad607532c2e8629de1153803e06afbba9451d3a09b3ec6e72330ade8f90c51161a6e4b844ed06557318b58ce9d32441a61a06e8a44c7abce301b";
     console.log("Expected Signature:", expectedSignature);
     console.log("Signatures match:", signature === expectedSignature);
 
